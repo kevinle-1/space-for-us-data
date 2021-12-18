d0fc10a825155d8b2141074832d1bc6cd057db33
Kevin Le <le.kevin838@gmail.com>
2021-12-18 18:24:01 +0800

    refactor: further testing and output

--- osm.py
+++ osm.py
@@ -19,9 +19,34 @@ fp = get_data("Canberra", directory=PBF_DIRECTORY_NAME)
 # init OSM parser object
 osm = OSM(fp)
 
+total_roads = 0
+roads_with_width = 0
+
+area = 0.0
+length = 0.0
+
 # plot roads
 drive_net = osm.get_network(network_type="driving")
 
-for road in drive_net.itertuples(index=True, name='Roads'):
-    print(road.name) # Road Name 16
+try:
+    for road in drive_net.itertuples(index=True, name='Roads'):
+        if(road.width): # for roads with width data
+            area += float(road.width) * float(road.length)
+            roads_with_width += 1
+        length += float(road.length)
+        total_roads += 1
+except Exception as e:
+    print(f"Error: {e}")
 
+print(area)
+print(length)
+print(total_roads)
+print(roads_with_width)
+print(total_roads - roads_with_width)
+
+# Output:
+# 137540.2
+# 4533967.0
+# 36962
+# 172
+# 36790 < Not many roads with width data - will have to rely heavily on estimates 
